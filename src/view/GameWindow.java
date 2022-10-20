5cefad4d6fb064841610ae001a701c5448743801
csabeszafiu <xrapidacy@gmail.com>
2022-10-21 00:07:11 +0200

    Worked on implementing a main menu for the game. > New Menu class (extends JPanel) > Added constants to the relevant class > Added code to the GameWindow class    - new local variable (stores the Menu itself)    - a few lines in the class' constructor    - put the constructor call inside invokeLater() for later asynchronous single threading purposes    - New- and Load Game buttons and a JLabel for the game's title (placeholders)

--- src/view/GameWindow.java
+++ src/view/GameWindow.java
@@ -15,9 +15,16 @@ public class GameWindow extends JFrame{
         setSize(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
         setVisible(true);setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
-        getContentPane().add(menu, BorderLayout.CENTER);
-        //pack();
+        //getContentPane().add(menu, BorderLayout.PAGE_START);
+        //will use different layout this is just for testing purposes while I figure out where I should efficiently store all these variables outside the constructor
         setLocationRelativeTo(null);
+        JButton startNewGame = new JButton("New game");
+        JButton loadGame = new JButton("Load game");
+        JLabel title = new JLabel("Valley of Stellar Dew", SwingConstants.CENTER);
+        getContentPane().add(title, BorderLayout.CENTER);
+        getContentPane().add(startNewGame, BorderLayout.PAGE_START);
+        getContentPane().add(loadGame, BorderLayout.PAGE_END);
+        //pack();
     }
 
 
