c530fdaad24b1aab6f58cdc49eb361b70c4133bf
Bodi Timea <qh5a9k@inf.elte.hu>
2022-12-21 17:23:36 +0100

    Implemented the buildings, the fences and the fields in the game.

--- src/model/GameBoard.java
+++ src/model/GameBoard.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import model.terrain.Buildings;
 import model.terrain.Cell;
 import model.terrain.Fields;
 import model.terrain.Grass;
@@ -15,7 +16,7 @@ public class GameBoard {
     Cell [][] board;
     private int width, height;
     private Player player;
-    private final String filename = "img/test_map2.csv";
+    private final String filename = "img/test_map.csv";
 
     public GameBoard(int width, int height){
         this.width = width;
@@ -67,7 +68,8 @@ public class GameBoard {
                                 board[y][x] = element;
                                 ++y;
                             } else {
-                                if(index.equals("34")){
+                                if(index.equals("34") || index.equals("10") || index.equals("11") || index.equals("12") || index.equals("13") || index.equals("14") || index.equals("25") || index.equals("26") || index.equals("27")
+                                || index.equals("28") || index.equals("29") || index.equals("40") || index.equals("41") || index.equals("42")){
                                     Cell element = new Cell(new Fields(num), x, y);
                                     board[y][x] = element;
                                     ++y;
@@ -82,9 +84,47 @@ public class GameBoard {
                                         board[y][x] = element;
                                         ++y;
                                     } else {
-                                        Cell element = new Cell(new Grass(16), x, y);
-                                        board[y][x] = element;
-                                        ++y;
+                                        if(index.equals("120") || index.equals("121") || index.equals("122") 
+                                        || index.equals("135") || index.equals("137") || index.equals("150") 
+                                        || index.equals("151") || index.equals("152") || index.equals("165")
+                                        || index.equals("167") || index.equals("180") || index.equals("181") 
+                                        || index.equals("182") || index.equals("183") || index.equals("184") 
+                                        || index.equals("185") || index.equals("186") || index.equals("187")
+                                        || index.equals("188") || index.equals("189") || index.equals("190")
+                                        || index.equals("191") || index.equals("195") || index.equals("196")
+                                        || index.equals("197") || index.equals("198") || index.equals("199")
+                                        || index.equals("200") || index.equals("201") || index.equals("202")
+                                        || index.equals("203") || index.equals("204") || index.equals("205")
+                                        || index.equals("206") || index.equals("210") || index.equals("211")
+                                        || index.equals("212") || index.equals("213") || index.equals("214")
+                                        || index.equals("215") || index.equals("216") || index.equals("217")
+                                        || index.equals("218") || index.equals("219") || index.equals("220")
+                                        || index.equals("221") || index.equals("225") || index.equals("226")
+                                        || index.equals("227") || index.equals("228") || index.equals("229")
+                                        || index.equals("230") || index.equals("231") || index.equals("232")
+                                        || index.equals("233") || index.equals("234") || index.equals("235")
+                                        || index.equals("236") || index.equals("240") || index.equals("241")
+                                        || index.equals("242") || index.equals("243") || index.equals("244")
+                                        || index.equals("245") || index.equals("246") || index.equals("247")
+                                        || index.equals("248") || index.equals("249") || index.equals("250")
+                                        || index.equals("251") || index.equals("255") || index.equals("256")
+                                        || index.equals("257") || index.equals("258") || index.equals("259")
+                                        || index.equals("260") || index.equals("261") || index.equals("262")
+                                        || index.equals("263") || index.equals("264") || index.equals("265")
+                                        || index.equals("266") || index.equals("274") || index.equals("275")
+                                        || index.equals("276") || index.equals("277") || index.equals("278")
+                                        || index.equals("279") || index.equals("280") || index.equals("281")
+                                        || index.equals("289") || index.equals("290") || index.equals("291")
+                                        || index.equals("292") || index.equals("293") || index.equals("294")
+                                        || index.equals("295") || index.equals("296")){
+                                            Cell element = new Cell(new Buildings(num), x, y);
+                                            board[y][x] = element;
+                                            ++y;
+                                        } else {
+                                            Cell element = new Cell(new Grass(16), x, y);
+                                            board[y][x] = element;
+                                            ++y;
+                                        }
                                     }
                                 }
                             }
--- src/model/terrain/Fields.java
+++ src/model/terrain/Fields.java
@@ -20,6 +20,58 @@ public class Fields extends GroundType {
                 case 34:
                     field = setImage("field.png");
                     break;
+
+                case 10:
+                    field = setImage("10.png");
+                    break;
+
+                case 11:
+                    field = setImage("11.png");
+                    break;
+                
+                case 12:
+                    field = setImage("12.png");
+                    break;
+
+                case 13:
+                    field = setImage("13.png");
+                    break;
+
+                case 14:
+                    field = setImage("14.png");
+                    break;
+
+                case 25:
+                    field = setImage("25.png");
+                    break;
+
+                case 26:
+                    field = setImage("26.png");
+                    break;
+
+                case 27:
+                    field = setImage("27.png");
+                    break;
+
+                case 28:
+                    field = setImage("28.png");
+                    break;
+
+                case 29:
+                    field = setImage("29.png");
+                    break;
+
+                case 40:
+                    field = setImage("40.png");
+                    break;
+
+                case 41:
+                    field = setImage("41.png");
+                    break;
+
+                case 42:
+                    field = setImage("42.png");
+                    break;
             }
             
         } catch (IOException e) {
